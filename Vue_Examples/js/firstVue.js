4ac424eac0bcd4ceaffa4b2d3e187993cb6c7d94
luedlo <luedlo97@gmail.com>
2019-05-09 14:00:06 -0600

    Fruit Store

--- Vue_Examples/js/firstVue.js
+++ Vue_Examples/js/firstVue.js
@@ -8,6 +8,52 @@ const app = new Vue({
 			{ name: 'Grape', quantity: 0 },
 			{ name: 'Pear', quantity: 80 },
 			{ name: 'Strawberries', quantity: 110 }
-		]
+		],
+		newFruit: '',
+		total: 0
+	},
+	methods: {
+		addFruit() {
+			// console.log("Click");
+			if (this.newFruit != "" ) {
+				this.fruits.push(this.newFruit);
+				this.fruits2.push({
+					name: this.newFruit,
+					quantity: 0
+				});
+				this.newFruit = '';
+			}
+		}
+	},
+	computed: {
+		sumFruits() {
+			this.total = 0;
+			for (fruit of this.fruits2) {
+				this.total += fruit.quantity;
+			}
+			return this.total;
+		}
 	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
